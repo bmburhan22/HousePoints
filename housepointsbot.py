f00c047361643bd4ea79a1cf976e46ef69578dc1
Burhanuddin Murtaza <bm.burhan22@gmail.com>
2023-08-27 18:15:21 +0530

    Change housepointsbot.py

--- housepointsbot.py
+++ housepointsbot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 
